0d34b261d33ec457b5b0988aa4ab200f000dd954
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-13 14:46:25 +0100

    Update cell divs to call new calcCellClass function

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,4 +1,5 @@
 import { Cell } from "../utils/createGrid";
+import { calcCellClass } from "../utils/cellStyle";
 
 export function Grid({ cells }: { cells: Cell[] }): JSX.Element {
   return (
@@ -7,7 +8,7 @@ export function Grid({ cells }: { cells: Cell[] }): JSX.Element {
         return (
           <div
             key={`${cell.pos.x},${cell.pos.y}`}
-            className={`cell ${cell.isMine ? "mine" : ""}`}
+            className={calcCellClass(cell)}
           >
             {!cell.isMine && cell.nearbyMines > 0 && cell.nearbyMines}
           </div>
