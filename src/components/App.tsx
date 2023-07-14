b2a5d02a4517745cbb695913308ac3887a4234be
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-14 13:32:58 +0100

    Add cell state changing for isHidden and isFlagged & left/right click handlers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,11 @@
-import { generateCells } from "../utils/createGrid";
+import {
+  Cell,
+  generateCells,
+  updatedFlagCell as getUpdatedFlagCell,
+  updatedHiddenCell as getUpdatedHiddenCell,
+} from "../utils/createGrid";
 import { Grid } from "./Grid";
-import { useMemo } from "react";
+import { useState } from "react";
 import "./App.css";
 
 const width = 10;
@@ -8,12 +13,43 @@ const height = 10;
 const mineChance = 20;
 
 function App() {
-  const cells = useMemo(() => generateCells(width, height, mineChance), []);
+  const [cells, setCells] = useState(generateCells(width, height, mineChance));
+
+  const handleRightClick = (cell: Cell) => {
+    setCells((prev) => {
+      const newCells = [...prev];
+      newCells[cell.id] = getUpdatedFlagCell(cell);
+
+      return newCells;
+    });
+  };
+
+  const handleLeftClick = (cell: Cell) => {
+    if (cell.isMine) {
+      // TODO: Handle loss
+
+      setCells(generateCells(width, height, mineChance));
+    } else {
+      setCells((prev) => {
+        const newCells = [...prev];
+        newCells[cell.id] = getUpdatedHiddenCell(cell);
+
+        return newCells;
+      });
+
+      // TODO: Check win
+      // TODO: Handle win
+    }
+  };
 
   return (
     <div className="App">
       <div className="grid-container">
-        <Grid cells={cells} />
+        <Grid
+          cells={cells}
+          handleRightClick={handleRightClick}
+          handleLeftClick={handleLeftClick}
+        />
       </div>
     </div>
   );
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,16 +1,38 @@
 import { Cell } from "../utils/createGrid";
 import { calcCellClass } from "../utils/cellStyle";
 
-export function Grid({ cells }: { cells: Cell[] }): JSX.Element {
+export function Grid({
+  cells,
+  handleLeftClick,
+  handleRightClick,
+}: {
+  cells: Cell[];
+  handleLeftClick: (cell: Cell) => void;
+  handleRightClick: (cell: Cell) => void;
+}): JSX.Element {
   return (
-    <div className="grid">
+    <div className="grid" onContextMenu={(e) => e.preventDefault()}>
       {cells.map((cell) => {
         return (
           <div
-            key={`${cell.pos.x},${cell.pos.y}`}
+            key={cell.id}
             className={calcCellClass(cell)}
+            onClick={() => handleLeftClick(cell)}
+            onKeyDown={() => handleLeftClick(cell)}
+            onContextMenu={(e) => {
+              e.preventDefault();
+              handleRightClick(cell);
+            }}
+            role="button"
+            tabIndex={cell.id}
           >
-            {!cell.isMine && cell.nearbyMines > 0 && cell.nearbyMines}
+            {cell.isFlagged
+              ? "🚩"
+              : cell.isHidden
+              ? ""
+              : cell.isMine
+              ? "💣"
+              : cell.nearbyMines > 0 && cell.nearbyMines}
           </div>
         );
       })}
